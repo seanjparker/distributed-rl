4de01ff2118528e8acbd0016b23cb47bd04da4b5
Sean Parker <sean.parker067@gmail.com>
2021-01-12 17:54:58 +0000

    feat: add ray

---
+++ algos/ray/ppo.py
@@ -0,0 +1,37 @@
+import ray
+import ray.rllib.agents.ppo as ray_ppo
+from ray.tune.logger import pretty_print
+from algos.common.util import EpochRecorder
+from time import time
+
+
+def ppo(current_workers, epochs):
+    ray.init()
+    config = ray_ppo.DEFAULT_CONFIG.copy()
+    config['num_gpus'] = 0
+    config['num_workers'] = current_workers
+    config['clip_param'] = 0.2
+    config['num_sgd_iter'] = 60
+    config['lambda'] = 0.97
+    config['framework'] = 'torch'
+
+    print(pretty_print(config))
+
+    trainer = ray_ppo.PPOTrainer(config=config, env="LunarLander-v2")
+
+    reward_rec = EpochRecorder(0, 'mean reward')
+
+    start_time = time()
+    for ep in range(epochs):
+        result = trainer.train()
+        avg_reward = result["episode_reward_mean"]
+        reward_rec.store(avg_reward)
+        print(f'epoch: {ep + 1}, mean reward: {avg_reward:.3f}')
+
+    tot_time = time() - start_time
+    reward_rec.dump(custom_data={
+        'framework': 'ray',
+        'd_lib': 'ray',
+        'workers': current_workers,
+        'time': str(tot_time)
+    })
--- launch.sh
+++ launch.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+while getopts f:e: flag
+do
+  case "${flag}" in
+    f) framework=${OPTARG};;
+    e) epochs=${OPTARG};;
+    *) echo "Invalid arguments";;
+  esac
+done
+
 for i in 2 4 8 16 32; do
-  python run.py --fw torch --w "$i" --epochs 200
+  python run.py --fw "$framework" --w "$i" --epochs "$epochs"
 done
